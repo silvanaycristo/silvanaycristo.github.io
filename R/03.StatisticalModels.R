64489bfb0e5683686b8c75ed73800f3560a4e758
silvanaycristo <silvanac@lcg.unam.mx>
2025-01-31 10:32:52 -0600

    Se añadieron ejemplos adicionales en el código.

--- R/03.StatisticalModels.R
+++ R/03.StatisticalModels.R
@@ -82,6 +82,39 @@ vd <- VisualizeDesign(
   designFormula = ~ Response + Response:ind.n + Response:Treatment,
   textSizeFitted = 3
 )
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
+
+## Interpreta ResponseResistant.Treatmentpre del ejercicio 2.
+
+## 1) Pre v.s post da el orden donde un valor positivo indica mayor expresión en el pre.
+## 2) Individuo constante.
+## 3) La respuesta es resistente.
+
+
+## ----Ejemplo 3 (adicional)-------------------
+
+(sampleData = data.frame(
+  condition = factor(rep(c("ctrl_minus", "ctrl_plus",
+                           "ko_minus", "ko_plus"), 3)),
+  batch = factor(rep(1:6, each = 2))))
+#>     condition batch
+#> 1  ctrl_minus     1
+#> 2   ctrl_plus     1
+#> 3    ko_minus     2
+#> 4     ko_plus     2
+#> 5  ctrl_minus     3
+#> 6   ctrl_plus     3
+#> 7    ko_minus     4
+#> 8     ko_plus     4
+#> 9  ctrl_minus     5
+#> 10  ctrl_plus     5
+#> 11   ko_minus     6
+#> 12    ko_plus     6
+vd <- VisualizeDesign(sampleData = sampleData,
+                      designFormula = ~ 0 + batch + condition,
+                      textSizeFitted = 4, lineWidthFitted = 20,
+                      dropCols = "conditionko_minus")
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
 
 ## ----EMM_example1_interactive, eval = FALSE-------------------
 # ## Usaremos shiny otra ves
